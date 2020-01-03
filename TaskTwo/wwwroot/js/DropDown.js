ef4a0f8629b2e049a51fb309ea29c1a553e3fc48
Carlos <carlosga2006@gmail.com>
2020-01-02 21:57:51 -0500

    Added table style

--- TaskTwo/wwwroot/js/DropDown.js
+++ TaskTwo/wwwroot/js/DropDown.js
@@ -1,7 +1,8 @@
-﻿function myMenu() {
+﻿//Menu
+function myMenu() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
-
+//FilterMenu
 function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
@@ -14,6 +15,26 @@ function filterFunction() {
             a[i].style.display = "";
         } else {
             a[i].style.display = "none";
+        }
+    }
+}
+
+//FilterTable
+function myFilterTable() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementsByClassName("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
     }
 }
